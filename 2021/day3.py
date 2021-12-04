7ec0349befbbaefefbb6f8ac89f3d714d499f9af
Sonex123 <53747556+Sonex123@users.noreply.github.com>
2021-12-05 00:58:42 +0100

    Update day3.py

--- 2021/day3.py
+++ 2021/day3.py
@@ -25,39 +25,5 @@ def part1():
 
 
 def part2():
-    with open("input") as f:
-        fr = f.read().split("\n")
-
-    oxygen = fr
-    co2 = fr
-
-    for i in range(len(fr[0])):
-        c1 = 0
-        c0 = 0
-        for j in fr:
-            if j[i] == "0":
-                c0 += 1
-            else:
-                c1 += 1
-        common = "1"
-        if c0 > c1:
-            common = "0"
-
-        if common == "0":
-            least = "1"
-        else:
-            least = "0"
-        for j in oxygen:
-            if j[i] == least and len(oxygen) != 1:
-                oxygen.remove(j)
-        for j in co2:
-            if j[i] == common and len(co2) != 1:
-                co2.remove(j)
-        print(i,c1,c0, least, common)
-        print(oxygen)
-        print(co2)
-
-    return int(oxygen[0],2) * int(co2[0],2)
-
-
-print(part2())
+    # Failed until moment
+    pass
